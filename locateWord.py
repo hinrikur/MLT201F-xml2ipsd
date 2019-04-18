fcab8aec59707028e7f5ed1ea6e05368f67e3ae8
hinrikur <hih43@hi.is>
2019-04-18 16:13:35 +0000

    More scripts

--- locateWord.py
+++ locateWord.py
@@ -1,12 +1,14 @@
 import os
 import xml.etree.ElementTree as et
+import sys
 import re
 import time
+from line_profiler import LineProfiler
 
 '''
 Hinrik Hafsteinsson - Vor 2019
 
-Skrifta sem ber saman lista af sagnorðum (hér heitir hún
+Skripta sem ber saman lista af sagnorðum (hér heitir hún
 'dat_acc_sagnir.txt') við .xml skrár í Risamálheild Árnastofnunar (RMH) og
 skilar setningu á token/tag/lemma formi ef viðkomandi sagnorð finnst í
 setningunni.
@@ -19,13 +21,13 @@ möppunum ÁR undirundirmöppunum MÁNUÐUR.
 '''    Breytur skilgreindar   '''
 ''' ========================= '''
 
-verbs = open('dat_acc_sagnir.txt', 'r').readlines()
+verbs = tuple(open('dat_acc_sagnir.txt', 'r').readlines())
+punctuation = ('!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~')
 
-# ATH þetta er absolute directory sem virkar ekki nema í minni vél.
-# settu dir á þinni eigin inntaksmöppu hér
+
+rmh_dir = sys.argv[0]
 rmh_dir = '/Users/hinrik/Documents/skoli/MA/vor_2019/MLT201F/morgunbladid_stuff/rmh_morgunbladid'
 
-rmh = os.listdir(rmh_dir)
 
 positive_xmls = []
 out_folder = 'match_sentences'
@@ -42,6 +44,7 @@ def make_dirs(target_dir):
     if os.path.isdir(target_dir):
         pass
     else:
+        print('Crateing output folder: "match_sentences"')
         os.mkdir(target_dir)
 
 def populate_dict(verb_lst):
@@ -54,7 +57,8 @@ def populate_dict(verb_lst):
         dict[verb.strip()] = 0
     return dict
 
-def match_verb(sentence, dict):
+# @profile
+def match_verb(sentence):
     '''
     Tekur inn setningu og skilar True ef sögn í sagnalistanum er í setningunni
     '''
@@ -63,12 +67,7 @@ def match_verb(sentence, dict):
         mark = word.get('type')
         for verb in verbs:
             verb_string = verb.strip()
-            # print(verb_string)
-            if verb_string == lemma:
-                # dict[verb_string] += 1
-                # print(verb, '->', dict[verb.strip()])
-                # return True
-                # print(verb_string)
+            if verb_string == lemma and mark[0] == 's':
                 return True
             else:
                 pass
@@ -81,65 +80,80 @@ def log_dict(dict):
     with open('verb_numbers.txt', 'w') as file:
         for k,v in dict.items():
             file.write(k + '\t' + str(v) + '\n')
-
+# # @profile
 def parse_sent(sentence):
     word_list = []
     for token in sentence:
         word_list.append(token.text)
     return word_list
-
+# @profile
 def string_sent(sentence):
     '''
-    Strengir saman token, tag og lemma með /
+    Strengir saman token, tag og lemma og prentar
+    Dæmi:
+        Því aa því
+        er sfg3en vera
+        meðal ae meðal
+        annars fohee annar
+        haldið sþghen halda
+        fram aa fram
+        . . .
     '''
     word_list = []
     for word in sentence:
-        word = str(word.text + '/' + word.get('type') + '/' + str(word.get('lemma')))
+        if word.text in punctuation: continue
+        if word.get('lemma') == None:
+            word = str(word.text + ' ' + word.text + ' ' + word.text)
+        else:
+            word = str(word.text + ' ' + word.get('type') + ' ' + str(word.get('lemma')))
         word_list.append(word)
     return word_list
 
+# # @profile
 def write_to_file(sentence_list, file):
-    file.write(' '.join(sentence_list))
+    file.write('\n'.join(sentence_list))
     file.write('\n\n')
 
-def traverse_subfolders(dir, dict):
-    # print(verb_string)
-    for year in dir:
-        if not year.startswith('.'):
-            year_path = os.path.join(rmh_dir, year)
-            # print(year_path)
-            for month in os.listdir(year_path):
-                if not month.startswith('.'):
-                    print('\nBegin:', month, '-', year)
-                    print('...')
-                    start = time.time()
-                    month_path = os.path.join(year_path, month)
-                    for xml in os.listdir(month_path):
-                        if not xml.endswith('.xml'): continue
-                        xml_file = os.path.join(month_path, xml)
-                        tree = et.parse(xml_file)
-                        root = tree.getroot()
-                        for sentence in root.iter('{http://www.tei-c.org/ns/1.0}s'):
-                            if match_verb(sentence, dict):
-                                out_file_name = str(year) + '.txt'
-                                out_file = open(out_file_name, 'a+')
-                                write_to_file(string_sent(sentence), out_file)
-                    end = time.time()
-                    print('End:', month, '-', year+'.', 'Time elapsed:', '%.2f' % (end - start), 'seconds.')
-                    print('Writing log to file')
-                    # add_to_file(month, year)
-                    # for k,v, in dict.items():
-                    #     print(k,v)
+# @profile
+def traverse_subfolders(in_dir):
+    print(in_dir)
+    for year in os.listdir(in_dir):
+        if year.startswith('.'): continue
+        print(year)
+        print('\nBegin:', year)
+        print('...')
+        start = time.time()
+        year_path = os.path.join(in_dir, year)
+        print(year_path)
+        for dir, months, files in os.walk(year_path, topdown=True):
+            for xml in files:
+                if not xml.endswith('.xml'): continue
+                xml_file = os.path.join(dir, xml)
+                # print(xml_file)
+                tree = et.parse(xml_file)
+                root = tree.getroot()
+                # dates = root.iter('{http://www.tei-c.org/ns/1.0}date')
+                # dates = [date.text for date in dates]
+                # doc_year = dates[1][:4]
+                for sentence in root.iter('{http://www.tei-c.org/ns/1.0}s'):
+                    if match_verb(sentence):
+                        out_file_name = str(year) + '.lemmatized'
+                        out_file = open(out_file_name, 'a+')
+                        write_to_file(string_sent(sentence), out_file)
+        end = time.time()
+        print('End:', year+'.', 'Time elapsed:', '%.2f' % (end - start), 'seconds.')
+        # print('Writing log to file')
+
 
 ''' ========================= '''
 '''        Föll keyrð         '''
 ''' ========================= '''
 
-verb_dict = populate_dict(verbs)
+# verb_dict = populate_dict(verbs)
 
 make_dirs(out_folder)
 os.chdir(out_dir)
 
-traverse_subfolders(rmh, verb_dict)
+traverse_subfolders(rmh_dir)
 
-log_dict(verb_dict)
+# log_dict(verb_dict)
