5de0ec77ee26e1e4b82807b1165dd8977dda5e8d
hinrikur <hih43@hi.is>
2019-04-23 15:10:36 +0000

    Duplicated main script

--- locateWord_minimized.py
+++ locateWord_minimized.py
@@ -3,15 +3,16 @@ import xml.etree.ElementTree as et
 import sys
 import re
 import time
+import subprocess
 from line_profiler import LineProfiler
 
 '''
 Hinrik Hafsteinsson - Vor 2019
 
-Skripta sem ber saman lista af sagnorðum (hér heitir hún
-'dat_acc_sagnir.txt') eða tiltekið sagnorð við .xml skrár í Risamálheild
-Árnastofnunar (RMH) og skilar setningu á token/tag/lemma formi ef viðkomandi
-sagnorð finnst í setningunni.
+Minnkuð útgáfa af locateWord.py.
+Leitar að tiltekinni sögn  við .xml skrár í Risamálheild Árnastofnunar (RMH) og
+skilar setningu á token/tag/lemma formi ef viðkomandi sagnorð finnst í
+setningunni.
 
 Inntak skriptunnar er hér miðað við undirmálheild RMH sem byggist upp af undir-
 möppunum ÁR undirundirmöppunum MÁNUÐUR.
@@ -21,20 +22,27 @@ möppunum ÁR undirundirmöppunum MÁNUÐUR.
 '''    Breytur skilgreindar   '''
 ''' ========================= '''
 
+# verbs = tuple(open('dat_acc_sagnir.txt', 'r').readlines())
 
-verbs = tuple(open('dat_acc_sagnir.txt', 'r').readlines())
+
+
+specific_verb = sys.argv[1]
 punctuation =  ('!', '"', '#', '$', '%', '&', '(', ')', '*',
                 '+', ',', '-', '/', ':', ';', '<', '=', '>',
                 '?', '@', '[', ']', '^', '_', '`', '{', '|',
                 '}', '~')
 
 cwd = os.getcwd()
+search_folder = sys.argv[2]
+search_dir = os.path.join('/Users/hinrik/Documents/skoli/MA/vor_2019/MLT201F/morgunbladid_stuff/', search_folder)
+# rmh_dir = '/Users/hinrik/Documents/skoli/MA/vor_2019/MLT201F/morgunbladid_stuff/rmh_morgunbladid'
 
-rmh_dir = sys.argv[0]
-rmh_dir = '/Users/hinrik/Documents/skoli/MA/vor_2019/MLT201F/morgunbladid_stuff/rmh_morgunbladid'
+# python3 locateWord_minimized.py gefa rmh_morgunbladid
 
+print(search_folder)
+print(search_dir)
 
-positive_xmls = []
+# positive_xmls = []
 out_folder = 'match_sentences'
 out_dir = os.path.join(cwd, out_folder)
 
@@ -47,10 +55,25 @@ def make_dirs(target_dir):
     Býr til möppu fyrir úttakskrár, ef hún er þegar ekki til.
     '''
     if os.path.isdir(target_dir):
+        print('Output folder "match_sentences" exists. Using that')
         pass
     else:
-        print('Crateing output folder: "match_sentences"')
+        print('Creating output folder: "match_sentences"')
         os.mkdir(target_dir)
+
+def make_sentFolder(year):
+    out_year = os.path.join(out_dir, year + '_lemmatized')
+    if os.path.isdir(out_year):
+        print('Output folder "{0}" exists. Using that'.format(out_year))
+        pass
+    else:
+        print('Creating output subfolder: "{0}"'.format(year + '_lemmatized'))
+        os.mkdir(out_year)
+
+def move_outputFile(file, year):
+    out_year = os.path.join(out_dir, year + '_lemmatized')
+    out_file = os.path.join(out_dir, file)
+    subprocess.call(["mv", out_file, out_year])
 
 # @profile
 def match_verb(sentence):
@@ -66,6 +89,18 @@ def match_verb(sentence):
                 return True
             else:
                 pass
+
+def match_specific_verb(sentence):
+    '''
+    Tekur inn setningu og skilar True ef tilgreind sögn er í setningunni
+    '''
+    for word in sentence:
+        lemma = word.get('lemma')
+        mark = word.get('type')
+        if specific_verb == lemma and mark[0] == 's':
+            return True
+        else:
+            continue
 
 # # @profile
 def parse_sent(sentence):
@@ -104,30 +139,36 @@ def write_to_file(sentence_list, file):
 
 # @profile
 def traverse_subfolders(in_dir):
-    print(in_dir)
+    sentence_counter = 0
     for year in os.listdir(in_dir):
+        out_file_number = 1
         if year.startswith('.'): continue
-        print(year)
         print('\nBegin:', year)
         print('...')
         start = time.time()
+        make_sentFolder(year)
         year_path = os.path.join(in_dir, year)
-        print(year_path)
         for dir, months, files in os.walk(year_path, topdown=True):
             for xml in files:
                 if not xml.endswith('.xml'): continue
                 xml_file = os.path.join(dir, xml)
-                # print(xml_file)
                 tree = et.parse(xml_file)
                 root = tree.getroot()
-                # dates = root.iter('{http://www.tei-c.org/ns/1.0}date')
-                # dates = [date.text for date in dates]
-                # doc_year = dates[1][:4]
                 for sentence in root.iter('{http://www.tei-c.org/ns/1.0}s'):
-                    if match_verb(sentence):
-                        out_file_name = str(year) + '.lemmatized'
+                    if match_specific_verb(sentence):
+                        sentence_counter += 1
+                        # print(sentence_counter)
+                        out_file_name = year + '_' + str(out_file_number) + '.lemmatized'
                         out_file = open(out_file_name, 'a+')
                         write_to_file(string_sent(sentence), out_file)
+                        if sentence_counter == 10000:
+                            print('Lemmatized file created:', out_file_name)
+                            out_file_number += 1
+                            sentence_counter = 0
+        for dir, folders, files in os.walk(out_dir, topdown=False):
+            for file in files:
+                if not file.endswith('.lemmatized'): continue
+                move_outputFile(file, year)
         end = time.time()
         print('End:', year+'.', 'Time elapsed:', '%.2f' % (end - start), 'seconds.')
         # print('Writing log to file')
---
+++ txt2ipsd-bolli.sh
@@ -0,0 +1,41 @@
+# echo "Encoding special markup"
+# python3 ./scripts/encodemarkup.py $1.tok $1.enc
+
+# echo "Tagging using IceTagger"
+# cat $1.enc | java -classpath "../IceNLP/IceNLPCore.jar" is.iclt.icenlp.runner.RunIceTagger > $1.tagged
+#
+# echo "Lemmatizing using Lemmald"
+# cat $1.enc | java -Xmx256m -classpath "../IceNLP/IceNLPCore.jar" is.iclt.icenlp.runner.RunIceTagger -of 1 -lem > $1.lemmatized
+
+# python3 ./scripts/joinlemma.py $1.tagged $1.lemmatized
+
+# Generate .tagged file from .lemmatized file
+awk '/ / {print $1 " " $2} !/ / {print $0}' $1.lemmatized > $1.taggedx # file where line breaks need fixing
+# changing single newlines to space and double newlines to single newlines
+awk '{printf "%s ", $0}' $1.taggedx | sed 's/  /\
+/g' > $1.tagged
+
+echo "Parsing using IceParser"
+cat $1.tagged | java -classpath "../IceNLP/IceNLPCore.jar" is.iclt.icenlp.runner.RunIceParser -f -l > $1.ipsdx
+
+echo "Converting IceParser's ipsd output to labeled bracketing"
+# Assumes .ipsd input and .psd output
+python3 ./scripts/ipsd2psd.py $1
+
+echo "Decoding special markup"
+python3 ./scripts/decodemarkup.py $1.ipsd $1.ipsd
+
+echo "Moving output files"
+mv $1.tagged $1.taggedx $1.ipsdx ../../xml2ipsd/parsing/intermediate_files
+# mv $1.lemmatized ../../sagnir_bolli/parsing/lemmatized
+mv $1.ipsd ../../xml2ipsd/parsing/parsed
+
+# ../../xml2ipsd/match_sentences/1999
+
+echo "Removing temporary files"
+# rm $1.enc
+# rm $1.tagged
+# rm $1.lemmatized
+# rm $1.ipsdx
+
+echo "Done"
