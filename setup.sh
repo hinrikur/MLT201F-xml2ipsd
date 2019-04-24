d22d74ed719f081e38fcdabb25978b22f8084e0c
hinrikur <hih43@hi.is>
2019-04-24 15:27:20 +0000

    setup script fix

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp parse_rmh_gefa.sh ./scripts/runall-OBJ.sh ./scripts/txt2ipsd-OBJ.sh ../icecorpus/parsing/
+cp ./scripts/parse_rmh_gefa.sh ./scripts/runall-OBJ.sh ./scripts/txt2ipsd-OBJ.sh ../icecorpus/parsing/
